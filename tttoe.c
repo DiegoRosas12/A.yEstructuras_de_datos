48b2bfaf477dcbbb3049a6fbb0c539b9d002173f
Diego Eduardo Rosas Gonzalez <diegoeduardo.rosas97@gmail.com>
2017-11-26 14:59:54 -0600

    posibilidad

--- tttoe.c
+++ tttoe.c
@@ -8,12 +8,20 @@
 
 
 //Funcion posibilidad
-
+int posibilidad(int T) {
+    int i, a=0;
+    //tenemos que guardar un resultado por renglón y al final sumar los resultados de los renglones
+    // para saber quien tiene más posibilidades de ganar
+    for (i=0;i<3;i++) {
+        a += T[i][k];
+    }
+    return 0;
+}
 
 
 int main(){
 int T[3][3];
-int i=0, k=0, p=0;
+int i=0, k=0, p=0, r;
 //Guardar posiciones
 for (i=0;i<3;i++){
     for(k=0;k<3;k++){
@@ -23,12 +31,14 @@ for (i=0;i<3;i++){
 }
 if (p>2||p<(-2)){
     printf("posición incorrecta\n");
+    r = -1;
 }else if (p==1) {
     printf("turno de O\n");
 }else if (p==-1) {
     printf("turno de X\n");
 }else {
     printf("No se sabe quien tira\n");
+   // r = 0;
 }
     //Imprimir tablero
     for(i=0;i<3;i++){
