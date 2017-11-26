8f071d028ab38149d940a3e5da7cff82667d1c9f
Diego Eduardo Rosas Gonzalez <diegoeduardo.rosas97@gmail.com>
2017-11-26 00:41:44 -0600

    .

--- tttoe.c
+++ tttoe.c
@@ -7,9 +7,11 @@ int main(){
 int T[3][3];
 int i, k;
 //Guardar posiciones
-for (i=0;i<3;i++)
-    for(k=0;k<3;k++)
+for (i=0;i<3;i++){
+    for(k=0;k<3;k++){
     scanf("%d",&T[i][k]);
+    }
+}
     //Imprimir tablero
     for(i=0;i<3;i++){
         for(k=0;k<3;k++){
