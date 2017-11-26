3a59bdf7cd6e10f3122e73924c85faf915b6afea
Diego Eduardo Rosas Gonzalez <diegoeduardo.rosas97@gmail.com>
2017-11-26 00:28:17 -0600

    Escanear y imprimir tablero

--- tttoe.c
+++ tttoe.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//funcion posibilidad
 
 int main(){
 int T[3][3];
-
+int i, k;
 //Guardar posiciones
-
+for (i=0;i<3;i++)
+    for(k=0;k<3;k++)
+    scanf("%d",&T[i][k]);
+    //Imprimir tablero
+    for(i=0;i<3;i++){
+        for(k=0;k<3;k++){
+        printf("%d",T[i][k]);
+        }
+        printf("\n");
+    }
 
 //Caso Incorrecto
 //Holaaaaa
