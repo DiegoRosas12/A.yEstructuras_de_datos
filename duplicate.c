96a2d167dfac029b0ab8b253ecff421fb9ce61c2
Jose Abel Vilchis Mar <joseabelvilchismar@MacBook-Pro-de-Jose.local>
2017-11-26 18:00:57 -0600

    comentario

--- duplicate.c
+++ duplicate.c
@@ -1,1 +1,17 @@
 // Find any duplicate
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+int n, *N;
+
+N = (int*)malloc(int*sizeof(int));
+
+printf("Ingrese el valor N\n");
+
+
+
+
+return 0;
+}
