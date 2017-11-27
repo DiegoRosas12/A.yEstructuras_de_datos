a7f08b1b95bfc4b14ecb6f06c65c15c1932af4e6
Jose Abel Vilchis Mar <joseabelvilchismar@MacBook-Pro-de-Jose.local>
2017-11-27 00:18:51 -0600

    comentario

--- duplicate.c
+++ duplicate.c
@@ -2,47 +2,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-
+ 
+ 
 int main(){
-    int n, i, j, aux=0, aux2=0, *N, *NX, *H;
-    
+    int n, i, *N, *NX, *H;
+   
     srand((unsigned int)time(NULL));
     printf("Ingrese el valor N\n");
     scanf("%d", &n);
     N = (int*)malloc((n+1)*sizeof(int));
     H = (int*)calloc(n, sizeof(int));
-    
-    
+
     NX = N;
-    
+
     while((NX-N)<(n+1)){
         *NX = ((n-1)*rand())/RAND_MAX+1;
         NX++;
     }
-    
+   
 //Histograma
     for(i = 0; i < (n+1); i++)
         H[N[i]-1]++;
 //Buscando la mayor repeticion
     for(i=0; i<(n+1); i++){
-        if(aux<H[i]){
-            aux = H[i];
-            aux2 = i+1; 
+        if(H[i]>1){
+        printf("\nEl numero que mas se repite es: %d\n", i+1);
+        break;
         }  
     }
-
-    printf("N (Desordenado)\n");
+ 
+   printf("N: %d\n", n);
     for(i=0; i<(n+1); i++){
         printf("%d ", N[i]);
     }
-    printf("\nEl numero que mas se repite es: %d\n", aux2);
-
-    printf("H (Histograma)\n");
+ 
+    printf("\nH (Histograma)\n");
     for(i=0; i<(n+1); i++){
         printf("%d ", H[i]);
     }
-    
+   
     free(H);
     free(N);
     return 0;
