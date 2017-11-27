6a4777a2e9249f04fe371a638b716581d22c9055
Diego Eduardo Rosas Gonzalez <diegoeduardo.rosas97@gmail.com>
2017-11-26 20:38:11 -0600

    maxDiff

--- maxDiff.c
+++ maxDiff.c
@@ -1,1 +1,14 @@
 //Max Difference
+#include <stdio.h>
+
+int main() {
+    int i,n, *p;
+    printf("Numero de elementos: ");
+    scanf("%i",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+        scanf("%i",&a[i]);
+    p = a;
+    
+    return 0;
+}
