427a9ac77354b7a03f0866b3b4474ddf15d59b5d
Jose Abel Vilchis Mar <joseabelvilchismar@MacBook-Pro-de-Jose.local>
2017-11-26 19:51:17 -0600

    comentario

--- duplicate.c
+++ duplicate.c
@@ -4,8 +4,32 @@
 #include <stdlib.h>
 #include <time.h>
 
+int* ordenar(int *N, int n, int **H){
+    int i, j;
+    int *ON, *aux, *H1;
+    H1 = *H
+    ON = (int*)malloc((n+1)*sizeof(int));
+
+    H1 == (int*)calloc(n, sizeof(int));
+
+    for(i = 0; i < (n+1); i++)
+        H1[N[i] - 1]++;
+    
+    j = max - 1;
+    for(i = 0; i <(n+1);)
+    {
+        while(H1[j])
+        {
+            ON[i++] = j + 1;
+            H1[j]--;
+        }
+        j--;
+    }
+    return ON;
+}
+
 int main(){
-int n, *N, *NX, *aux, *H;
+int n, i, *N, *NX, *aux, *H;
 
 srand(time(NULL));
 printf("Ingrese el valor N\n");
@@ -24,9 +48,24 @@ H = NULL;
 aux = N;
 while(aux - N < (n+1)){
     NX[aux - N] = ordenar(aux, n, &H)
-
     aux++;
 }
+
+printf("N (Desordenado) = %d\n", n);
+for(i=0; i<(n+1); i++){
+    printf("%d ", N[i]);
+}
+
+printf("NX (Ordenado)\n");
+for(i=0; i<(n+1); i++){
+    printf("%d ", NX[i]);
+}
+
+printf("H (Histograma)\n", n);
+for(i=0; i<n; i++){
+    printf("%d ", H[i]);
+}
+
 
 free(H);
 free(N);
