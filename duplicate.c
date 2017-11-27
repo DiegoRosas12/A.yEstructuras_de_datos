d67b7da6c6a263cbf6a70326733cd8d180496e0c
Jose Abel Vilchis Mar <joseabelvilchismar@MacBook-Pro-de-Jose.local>
2017-11-26 18:47:11 -0600

    comentario

--- duplicate.c
+++ duplicate.c
@@ -2,16 +2,25 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(){
-int n, *N;
+int n, *N, *NX, *H;
 
+srand(time(NULL));
 printf("Ingrese el valor N\n");
 scanf("%d", &n);
 N = (int*)malloc((n+1)*sizeof(int));
+H = (int*)calloc(n, sizeof(int));
+
+NX = N;
+
+while(NX-N<(n+1)){
+    *NX = ((n-1)*rand()/RAND_MAX+1);
+    NX++;
+}
 
 
-
-
+free(N);
 return 0;
 }
