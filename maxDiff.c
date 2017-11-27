ecf0d133917686598f887ce9eebd9bf806aceb20
Diego Eduardo Rosas Gonzalez <diegoeduardo.rosas97@gmail.com>
2017-11-26 21:50:46 -0600

    introduccion de algoritmo de intercambio

--- maxDiff.c
+++ maxDiff.c
@@ -1,33 +1,40 @@
 //Max Difference
 #include <stdio.h>
+#include <stdlib.h>
 
-int** sort(int *x, int n){
+int** sort(int *p, int n){
 	int i, j;
 	int **px, *aux;
 	px = (int**)malloc(n*sizeof(int*));
-	for (i = 1, *px = x++; i < n; i++, x++)
-		px[i] = x;
-	for (i = 0; i < n - 1; i++)
-		for (j = i + 1; j < n; j++)
-			if (*px[i] < *px[j])
-			{
-				aux = px[i];
-				px[i] = px[j];
-				px[j] = aux;
-			}
+	for (i = 1, *px = p++; i < n; i++, p++)
+		px[i] = p;
+        for (i=1; i<n; i++) {
+          j = i - 1;
+          aux = px[i];
+          while(*aux < *px[j]){
+               px[j+1] = px[j];
+               j--;
+               if (j == -1)
+                    break;
+          }
+          px[j+1] = aux; 
+     }
+    
 	return px;
 }
 
 int main() {
-    int i,n, *p;
+    int i,n, *p, **px;
     printf("Numero de elementos: ");
     scanf("%i",&n);
     int a[n];
     for(i=0;i<n;i++)
         scanf("%i",&a[i]);
     p = a;
+    px = sort(p,n);
+    printf("\n");
     for(i=0;i<n;i++)
-    printf("%i\n",*(p+i));
-
+    printf("%i\n",**(px+i));
+    free(px);
     return 0;
 }
